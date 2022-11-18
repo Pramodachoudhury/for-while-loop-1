4bb311256dc78b2f6eeeeae7162ddaf0846a1364
Pramoda Kumar choudhury <108170180+Pramodachoudhury@users.noreply.github.com>
2022-11-18 16:37:12 +0530

    Add files via upload

---
+++ for&while loop 1.py
@@ -0,0 +1,73 @@
+#wap to print numbers which have divisable by 3 from 1 to 10?
+"""i=int(input('enter a number:'))
+s=0
+
+for i in range(1,11):
+    if i%3==0:
+        print(i)"""
+"""i=int(input('enter a number:'))
+while i<=10:
+    if i%3==0:
+        print(i)
+    i=i+1"""
+#ask the user to enter any positive integer number and your program has to give
+#the sum of all positive integer 'n'?
+"""n=int(input('enter a number:'))
+s=0
+for n in range(1,n+1):
+    s=s+n
+    print('the value of sum is {} the iretation is {}'.format(n,s))
+print('the value of sum is {} the iretation is {}'.format(n,s))"""
+
+"""n=int(input('enter a number:'))
+s=0
+i=1
+while i<=n:
+    s=s+i
+    print('the value is {} the iteration {}'.format(i,s))
+    i=i+1"""
+#Program to find the sum of all numbers stored in a list?
+"""lists=[1,2,3,4,5,6]
+sum=0
+for values in lists:
+    sum=sum+values
+    print('the no of list {} the sum is {}'.format(list,sum))"""
+#WAP to print pattern?
+"""sum=5
+for i in range(1,sum+1,1):
+    for j in range(1,i+1):
+        print(j, end=' ')
+    print("")"""
+"""n=2
+for i in range(1,11):
+    product=n*i
+    print(product)"""
+#wap to print 2 table?
+"""n=1
+for i in range(1,11):
+    print('{} * {}={}'.format(2,i,2*i))
+    i=i+1
+n=1
+while n<=10:
+    print('{} * {} ={}'.format(2,n,2*n))
+    n+=1"""
+#wap to print any table by user input?
+"""n=int(input('enter a no:'))
+for i in range(1,11):
+    print('{} * {}={}'.format(n,i,n*i))
+    i+=1
+i=1
+while i<=10:
+    print('{} * {}={}'.format(n,i,n*i))
+    i+=1"""
+#wap to find the factorial of user input?
+"""n=int(input('enter a number:'))
+f=1
+for i in range(1,n+1):
+    f=f*n
+    print(f)
+i=1
+while i<=n:
+    f=f*n
+    i=i+1
+    print(f)"""
